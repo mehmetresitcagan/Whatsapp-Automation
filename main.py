5469644e7e4fb611d6cd92b91d7f4b57a6095801
mehmetresitcagan <mresitcagan@gmail.com>
2021-08-28 20:15:12 +0300

    send message instantly

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import pandas as pd
-
+import pywhatkit as kit
+import time
 
 def read_contact_file():
     data = pd.read_excel(io="contact.xlsx")
-    for i in data["contact"]:
-        print(i)
+
+    dataDict = {}
+
+    for name, contact in zip(data["name"], data["contact"]):
+        dataDict[name] = contact
+        kit.sendwhatmsg_instantly(phone_no="+90"+str(contact),message="selam "+name,wait_time=20)
+
+
 
 
 if __name__ == '__main__':
